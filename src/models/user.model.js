28ea96c07d85bb8ff39c430349a7b9c992f9ba79
sraveshT7 <sarveshtalekar123@gmail.com>
2024-07-23 20:03:55 +0530

    file upload setup - multer and cloudinary

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -32,7 +32,7 @@ const userSchema = new Schema({
 },
     { timestamps: true });
 
-userSchema.pre("save", async function () {
+userSchema.pre("save", async function (next) {
     if (!this.password.isModified("password")) return next()
 
     this.password = bcrypt.hash(this.password, 10)
---
+++ utils/cloudinary.js
@@ -0,0 +1,41 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+});
+
+const cloudinaryUploader = async (localfilepath) => {
+    try {
+        if (!localfilepath) return null
+        // uploading file on cloudinary
+        const response = await cloudinary.uploader.upload(localfilepath, { resource_type: "auto" })
+
+        //file has been uploaded successfully
+        console.log("response of upload", response)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localfilepath) // remove the locally saved temporary file as the operation got failed
+    }
+
+}
+export { cloudinaryUploader }
+
+
+
+
+// Upload an image
+const uploadResult = await cloudinary.uploader
+    .upload(
+        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+        public_id: 'shoes',
+    }
+    )
+    .catch((error) => {
+        console.log(error);
+    });
+
+console.log(uploadResult)
